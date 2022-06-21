588cd463c3a339f0a92f6e89acf91f049782422b
Alex-Rafter <you@example.com>
2022-06-21 17:30:32 +0100

    added spots and changed img ref

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
 
 <main class="main overflow-hidden" id="main">
     <?php include('./inc/modules/home/home-hero-v3.php'); ?>
+    <?php include('./inc/modules/home/chunky-spotlights.php'); ?>
 </main>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
