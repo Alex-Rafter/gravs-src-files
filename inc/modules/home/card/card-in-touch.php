a09f0e405942483e27a892f3a525c3e03d9bffe4
Alex-Rafter <you@example.com>
2022-06-23 14:04:11 +0100

    get in touch static

---
+++ inc/modules/home/card/card-in-touch.php
@@ -0,0 +1,12 @@
+<div class="get-in-touch flex-c">
+    <div class="in-touch-action bg-light flex-c p-5">
+        <div class="in-touch-action__icon-circle bg-primary rounded-circle flex-c">
+            <i class="fa-thin fa-phone text-white fs-4 p-4"></i>
+        </div>
+        <div class="flex-c flex-wrap h-100 p-3">
+            <p class="w-100 mb-0">Start a conversation</p>
+            <h6 class="w-100 mb-0">ON LIVE CHAT</h6>
+
+        </div>
+    </div>
+</div>
---
+++ inc/modules/home/get-in-touch.php
@@ -0,0 +1,29 @@
+<div class="container-fluid px-0">
+    <div class="row">
+        <div class="col-12 col-md-6 px-md-0">
+            <img class="resp-img" src="/img/home-about/home-about.jpg" alt="home about">
+        </div>
+        <div class="col-12 col-md-6 px-md-0">
+            <div
+                class="in-touch-form flex-c-col bg-primary text-white p-5 py-6 p-md-7 px-lg-6 py-lg-6 p-xl-7 p-xxl-8 p-xxxl-8 mb-0 flex-c-col justify-content-center align-items-start flex-wrap h-100">
+                <h2 class="home-locations__title text-white fs-xl-1 lh-sm">GET IN TOUCH</h2>
+                <p class="home-locations__paragraph">
+                With your chosen dealer & we'll contact you as soon as possible.
+                </p>
+                <input type="text" placeholder="your postcode"
+                    class="form-control w-100 w-sm-50 w-lg-100 w-xxl-50 mb-2" />
+                <input type="submit" class="btn btn-primary border border-white" text="REQUEST CALL BACK" />
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid px-0">
+    <div class="get-in-touch flex-c bg-white gap-3 p-3">
+        <?php include('./inc/modules/home/card/card-in-touch.php'); ?>
+        <?php include('./inc/modules/home/card/card-in-touch.php'); ?>
+        <?php include('./inc/modules/home/card/card-in-touch.php'); ?>
+        <?php include('./inc/modules/home/card/card-in-touch.php'); ?>
+        <?php include('./inc/modules/home/card/card-in-touch.php'); ?>
+
+    </div>
+</div>
