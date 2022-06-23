1248fccd232acf3b7bd1380f8cc9e4bb84525935
Alex-Rafter <you@example.com>
2022-06-23 16:20:11 +0100

    tweaks to resp styles for get in touch

--- inc/modules/home/card/card-in-touch.php
+++ inc/modules/home/card/card-in-touch.php
@@ -1,9 +1,9 @@
 <div class="get-in-touch flex-c">
-    <div class="in-touch-action bg-light flex-c p-5">
+    <div class="in-touch-action bg-light flex-c p-1 p-xl-3 p-xxl-5">
         <div class="in-touch-action__icon-circle bg-primary rounded-circle flex-c">
-            <i class="fa-thin fa-phone text-white fs-4 p-4"></i>
+            <i class="fa-thin fa-phone text-white fs-6 fs-md-4 p-4"></i>
         </div>
-        <div class="flex-c flex-wrap h-100 p-3">
+        <div class="flex-c flex-wrap h-100 p-3 d-none d-xl-flex">
             <p class="w-100 mb-0">Start a conversation</p>
             <h6 class="w-100 mb-0">ON LIVE CHAT</h6>
 
--- inc/modules/home/get-in-touch.php
+++ inc/modules/home/get-in-touch.php
@@ -7,17 +7,16 @@
             <div
                 class="in-touch-form flex-c-col bg-primary text-white p-5 py-6 p-md-7 px-lg-6 py-lg-6 p-xl-7 p-xxl-8 p-xxxl-8 mb-0 flex-c-col justify-content-center align-items-start flex-wrap h-100">
                 <h2 class="home-locations__title text-white fs-xl-1 lh-sm">GET IN TOUCH</h2>
-                <p class="home-locations__paragraph">
+                <p class="home-locations__paragraph mb-0">
                     With your chosen dealer &amp; we'll contact you as soon as possible.
                 </p>
-                <div class="flex-c gap-4 text-uppercase mb-3">
+                <div class="flex-c gap-4 text-uppercase my-4">
                     <div class="form-check">
                         <input class="form-check-input" type="checkbox" value="" id="checkOne">
                         <label class="form-check-label small" for="checkOne">
                             Callback
                         </label>
                     </div>
-
                     <div class="form-check">
                         <input class="form-check-input" type="checkbox" value="" id="checkTwo">
                         <label class="form-check-label small" for="checkTwo">
@@ -25,33 +24,29 @@
                         </label>
                     </div>
                 </div>
-
                 <div class="flex-c flex-wrap flex-sm-nowrap flex-md-wrap flex-lg-nowrap gap-sm-2 mb-md-2 mb-lg-0">
-
                     <input type="text" placeholder="DEALERSHIP"
                         class="form-control w-100 w-sm-50 w-md-100 w-lg-50 w-xxl-50 mb-2">
                     <input type="text" placeholder="YOUR NAME"
                         class="form-control w-100 w-sm-50 w-md-100 w-lg-50 w-xxl-50 mb-2">
                 </div>
-                <div class="flex-c flex-wrap flex-sm-nowrap flex-md-wrap flex-lg-nowrap gap-sm-2 mb-3">
+                <div class="flex-c flex-wrap flex-sm-nowrap flex-md-wrap flex-lg-nowrap gap-sm-2 mb-4">
                     <input type="text" placeholder="EMAIL"
                         class="form-control w-100 w-sm-50 w-md-100 w-lg-50 w-xxl-50 mb-2">
                     <input type="text" placeholder="PHONE"
                         class="form-control w-100 w-sm-50 w-md-100 w-lg-50 w-xxl-50 mb-2">
                 </div>
-
-                <input type="submit" class="btn btn-primary border border-white" text="REQUEST CALL BACK">
+                <input type="submit" class="btn btn-primary border border-white" value="REQUEST CALL BACK">
             </div>
         </div>
     </div>
 </div>
 <div class="container-fluid px-0">
-    <div class="get-in-touch flex-c bg-white gap-3 p-3">
+    <div class="get-in-touch flex-c justify-content-around justify-content-md-center bg-light bg-xl-white gap-0 gap-md-6 gap-xl-3 p-3">
         <?php include('./inc/modules/home/card/card-in-touch.php'); ?>
         <?php include('./inc/modules/home/card/card-in-touch.php'); ?>
         <?php include('./inc/modules/home/card/card-in-touch.php'); ?>
         <?php include('./inc/modules/home/card/card-in-touch.php'); ?>
         <?php include('./inc/modules/home/card/card-in-touch.php'); ?>
-
     </div>
 </div>
