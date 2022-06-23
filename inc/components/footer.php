ea9db27eec29aab5afbd2d28a8951fc0948b7465
Alex-Rafter <you@example.com>
2022-06-23 13:38:12 +0100

    footer static

---
+++ inc/components/footer.php
@@ -0,0 +1,118 @@
+<footer class="footer p-6 p-lg-7 p-xl-8 bg-light">
+    <div class="container px-0 mx-0">
+        <div class="row">
+            <div class="col-6 col-md-2">
+                <h5 class="text-primary text-sm">All Brands</h5>
+                <ul class="d-flex flex-column nav">
+                    <li class="mb-1"><a href="/used-car-results/audi/-1.aspx">AUDI (1)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/bmw/-1.aspx">BMW (1)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/citroen/-1.aspx">CITROEN (2)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/dacia/-1.aspx">DACIA (54)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/ds/-1.aspx">DS (1)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/ford/-1.aspx">FORD (5)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/honda/-1.aspx">HONDA (2)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/hyundai/-1.aspx">HYUNDAI (3)</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md-2">
+                <h5 class="text-primary text-sm">DACIA</h5>
+                <ul class="d-flex flex-column nav">
+                    <li class="mb-1"><a href="/used-car-results/audi/-1.aspx">AUDI (1)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/bmw/-1.aspx">BMW (1)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/citroen/-1.aspx">CITROEN (2)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/dacia/-1.aspx">DACIA (54)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/ds/-1.aspx">DS (1)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/ford/-1.aspx">FORD (5)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/honda/-1.aspx">HONDA (2)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/hyundai/-1.aspx">HYUNDAI (3)</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md-2">
+                <h5 class="text-primary text-sm">KIA</h5>
+                <ul class="d-flex flex-column nav">
+                    <li class="mb-1"><a href="/used-car-results/audi/-1.aspx">AUDI (1)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/bmw/-1.aspx">BMW (1)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/citroen/-1.aspx">CITROEN (2)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/dacia/-1.aspx">DACIA (54)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/ds/-1.aspx">DS (1)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/ford/-1.aspx">FORD (5)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/honda/-1.aspx">HONDA (2)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/hyundai/-1.aspx">HYUNDAI (3)</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md-2">
+                <h5 class="text-primary text-sm">RENAULT</h5>
+                <ul class="d-flex flex-column nav">
+                    <li class="mb-1"><a href="/used-car-results/audi/-1.aspx">AUDI (1)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/bmw/-1.aspx">BMW (1)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/citroen/-1.aspx">CITROEN (2)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/dacia/-1.aspx">DACIA (54)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/ds/-1.aspx">DS (1)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/ford/-1.aspx">FORD (5)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/honda/-1.aspx">HONDA (2)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/hyundai/-1.aspx">HYUNDAI (3)</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md-2">
+                <h5 class="text-primary text-sm">SEAT</h5>
+                <ul class="d-flex flex-column nav">
+                    <li class="mb-1"><a href="/used-car-results/audi/-1.aspx">AUDI (1)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/bmw/-1.aspx">BMW (1)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/citroen/-1.aspx">CITROEN (2)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/dacia/-1.aspx">DACIA (54)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/ds/-1.aspx">DS (1)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/ford/-1.aspx">FORD (5)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/honda/-1.aspx">HONDA (2)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/hyundai/-1.aspx">HYUNDAI (3)</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md-2">
+                <h5 class="text-primary text-sm">IMPORTANT</h5>
+                <ul class="d-flex flex-column nav">
+                    <li class="mb-1"><a href="/used-car-results/audi/-1.aspx">AUDI (1)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/bmw/-1.aspx">BMW (1)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/citroen/-1.aspx">CITROEN (2)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/dacia/-1.aspx">DACIA (54)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/ds/-1.aspx">DS (1)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/ford/-1.aspx">FORD (5)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/honda/-1.aspx">HONDA (2)</a></li>
+                    <li class="mb-1"><a href="/used-car-results/hyundai/-1.aspx">HYUNDAI (3)</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="row mt-6">
+            <div class="col-12 col-xl-7">
+                <h5 class="text-primary text-sm">REGISTERED</h5>
+                <address class="mb-1">
+                    SERVICE CENTRE PEMBREY ROAD, KIDWELLY, CARMARTHENSHIRE SA17 4TF <br>
+                </address>
+                <span>REG NO: 00653461 . VAT NO: 0000000</span>
+            </div>
+        </div>
+    </div>
+</footer>
+<footer class="bottom-footer bg-white py-5">
+    <div class="container">
+        <div class="flex-c justify-content-start">
+            <a href="" class="me-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="bi bi-facebook" viewBox="0 0 16 16">
+                    <path
+                        d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05h5.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z">
+                    </path>
+                </svg>
+            </a>
+            <a href="" class="mx-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter"
+                    viewBox="0 0 16 16">
+                    <path
+                        d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
+                </svg>
+            </a>
+
+            <p class="mb-0 ms-2 small" style="transform: translateY(1px);">© 2022 GRAVELLS - AUTOMOTIVE WEBSITE PROVIDED
+                BY BLUESKY INTERACTIVE LTD</p>
+
+        </div>
+    </div>
+</footer>
