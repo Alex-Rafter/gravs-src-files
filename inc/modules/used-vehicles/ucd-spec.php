d521910a667bb69b2ce047af67666f65f76d4eeb
Alex-Rafter <you@example.com>
2022-06-28 16:50:49 +0100

    ucd spec mob

--- inc/modules/used-vehicles/ucd-spec.php
+++ inc/modules/used-vehicles/ucd-spec.php
@@ -1,13 +1,14 @@
-<section class="ucd-spec p-6 p-lg-7 p-xl-10 bg-light">
+<section class="ucd-spec p-4 p-lg-7 p-xl-10 bg-light">
     <div class="container px-0 mx-0">
         <div class="row">
             <div class="col-12">
-            <div class="ucd-spec__titles mb-6">
+            <div class="ucd-spec__titles mb-6 d-none d-xl-block">
                     <h2 class="ucd-hero__title h1 mb-0">2017 RENAULT CAPTUR</h2>
                     <h3 class="ucd-hero_sub-title fs-5 fw-light mb-0">1.5 DYNAMIQUE NAV DCI</h3>
                 </div>
                 
-                <div class="ucd-spec__icon-overview flex-c justify-content-start border-top border-bottom border-primary gap-4 py-5 small">
+                <div class="ucd-spec__icon-overview flex-c justify-content-start border-top border-bottom border-grey border-xl-primary gap-4 py-3 py-xl-5 small">
+                    <!-- Make this a slider on mob -->
                     <span><i class="text-primary fa-thin fa-arrows-maximize ms-1"></i> 58,000 MILES</span>
                     <span><i class="text-primary fa-thin fa-arrows-maximize ms-1"></i> 58,000 MILES</span>
                     <span><i class="text-primary fa-thin fa-arrows-maximize ms-1"></i> 58,000 MILES</span>
@@ -20,7 +21,8 @@
                     <span><i class="text-primary fa-thin fa-arrows-maximize ms-1"></i> 58,000 MILES</span>
                     <span><i class="text-primary fa-thin fa-arrows-maximize ms-1"></i> 58,000 MILES</span>
                 </div>
-                <div class="ucd-spec__full-spec px-xl-5 border-bottom border-primary">
+                <div class="ucd-spec__full-spec px-5 border-bottom border-primary">
+                    <!-- Make this a slider on mob -->
                     <div class="row my-5">
                         <div class="col-12 col-xl-4"><ul class="list-group">
                             <li class="list-group-item">list item</li>
@@ -34,7 +36,7 @@
                             <li class="list-group-item">list item</li>
                             <li class="list-group-item">list item</li>
                         </ul></div>
-                        <div class="col-12 col-xl-4"><ul class="list-group">
+                        <div class="col-12 col-xl-4 d-none d-xl-block"><ul class="list-group">
                             <li class="list-group-item">list item</li>
                             <li class="list-group-item">list item</li>
                             <li class="list-group-item">list item</li>
@@ -46,7 +48,7 @@
                             <li class="list-group-item">list item</li>
                             <li class="list-group-item">list item</li>
                         </ul></div>
-                        <div class="col-12 col-xl-4"><ul class="list-group">
+                        <div class="col-12 col-xl-4 d-none d-xl-block"><ul class="list-group">
                             <li class="list-group-item">list item</li>
                             <li class="list-group-item">list item</li>
                             <li class="list-group-item">list item</li>
@@ -60,9 +62,9 @@
                         </ul></div>
                     </div>
                 </div>
-                <div class="ctas mt-5 flex-c justify-content-start gap-2">
-                        <a href="" class="btn btn-primary">ENQUIRE NOW</a>
-                        <a href="" class="btn btn-secondary border-primary">TEST DRIVE</a>
+                <div class="ctas mt-5 flex-c flex-wrap justify-content-start gap-2">
+                        <a href="" class="btn btn-primary w-75 w-sm-auto">ENQUIRE NOW</a>
+                        <a href="" class="btn btn-secondary border-primary w-75 w-sm-auto">TEST DRIVE</a>
                     </div>
             </div>            
         </div>
@@ -78,5 +80,12 @@
         left: -1rem;
         font-family: "Font Awesome 6 Pro";
         font-weight: 100;
+    }
+
+    @media only screen and (max-width: 1199px) {
+        .ucd-spec li.list-group-item {
+        position: relative;
+        padding: 0.3rem 1rem;
+    }
     }
 </style>
