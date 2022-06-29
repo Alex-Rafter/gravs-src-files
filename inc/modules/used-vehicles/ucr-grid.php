18c7c435ca8d5068ee9f8ce7ead5a7a504fff357
Alex-Rafter <you@example.com>
2022-06-29 12:08:19 +0100

    ucr inc offer card

---
+++ inc/modules/used-vehicles/ucr-grid.php
@@ -0,0 +1,16 @@
+<section class="ucr-grid container-fluid bg-white py-6 px-9">
+  <div class="row gx-2 gx-xl-6">
+    <div class="col-12 col-lg-3">
+    <?php include('./inc/modules/home/card/card-vehicle-offer.php'); ?>
+    </div>
+    <div class="col-12 col-lg-3">
+    <?php include('./inc/modules/home/card/card-vehicle.php'); ?>
+    </div>
+    <div class="col-12 col-lg-3">
+    <?php include('./inc/modules/home/card/card-vehicle.php'); ?>
+    </div>
+    <div class="col-12 col-lg-3">
+    <?php include('./inc/modules/home/card/card-vehicle.php'); ?>
+    </div>
+  </div>
+</section>
---
+++ inc/modules/used-vehicles/ucr-search.php
@@ -0,0 +1,11 @@
+<section class="ucr-search container-fluid bg-light py-7">
+  <div class="row">
+    <div class="col-12">
+      <div class="ucr-search__search text-center flex-c-col">
+      <h1 class="display-3 mb-0">YOUR SEARCH RESULTS</h1>
+        <p>We currently have 642 vehicles that match your search.</p>
+        <input type="text" class="form-control bg-white w-100 w-md-75 w-lg-50 w-xxxl-25">
+      </div>
+    </div>
+  </div>
+</section>
