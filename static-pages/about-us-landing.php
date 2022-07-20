31d711aa6b158519d8da488abdbda4474ac69657
Alex-Rafter <you@example.com>
2022-07-20 12:21:09 +0100

    last save before renault

--- static-pages/about-us-landing.php
+++ static-pages/about-us-landing.php
@@ -12,7 +12,7 @@
 </head>
 
 <body>
-    <?php include('/src-test/inc/components/header.php'); ?>
+    <?php include('/inc/components/header.php'); ?>
 
     <main class="main overflow-hidden bg-light" id="main">
 
@@ -28,12 +28,12 @@
             $landingPageTiles[4] = $arrCreate("Meet the team","/img/about-us/landing/meet-the-team-wide.jpg");
         ?>
 
-        <?php include('/src-test/inc/modules/landing-page/landing-hero-dynamic.php'); ?>
-        <?php include('/src-test/inc/modules/landing-page/equal-grid.php'); ?>
-        <?php include('/src-test/inc/modules/service-booking/s1-cms-section.php'); ?>
-        <?php include('/src-test/inc/modules/home/get-in-touch.php'); ?>
+        <?php include('/inc/modules/landing-page/landing-hero-dynamic.php'); ?>
+        <?php include('/inc/modules/landing-page/equal-grid.php'); ?>
+        <?php include('/inc/modules/service-booking/s1-cms-section.php'); ?>
+        <?php include('/inc/modules/home/get-in-touch.php'); ?>
     </main>
-    <?php include('/src-test/inc/components/footer.php'); ?>
+    <?php include('/inc/components/footer.php'); ?>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
