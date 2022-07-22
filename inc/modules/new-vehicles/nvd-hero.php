0b3944f8f43717d52391d84f7c74eff257bc47d2
Alex-Rafter <you@example.com>
2022-07-22 11:29:54 +0100

    ren nvd hero inc icons actions

--- inc/modules/new-vehicles/nvd-hero.php
+++ inc/modules/new-vehicles/nvd-hero.php
@@ -5,11 +5,83 @@
         <div class="col-12 col-lg-6 px-0 px-xl-4">
             <div class="home-image">
                 <img class="resp-img pb-0" src="/img/home-about/home-about.jpg" alt="home about"
-                style="aspect-ratio: 6/4;padding: 2rem 2rem 2rem 0;object-fit: cover;">
-                <div class="home-image-actions arrows-append-mob flex-c justify-content-xl-start mt-4 gap-3 text-uppercase position-relative pe-5 pb-5">
-                    <span class=""><a href="/" class="text-decoration-none small">Brochure Download <i class="fs-6 text-primary fa-thin fa-camera ms-1"></i></a></span>
-                    <span class=""><a href="/" class="text-decoration-none small">Available in Hybrid <i class="fs-6 text-primary fa-thin fa-camera ms-1"></i></a></span>
-                    <span class=""><a href="/" class="text-decoration-none small">Build Your Xceed <i class="fs-6 text-primary fa-thin fa-camera ms-1"></i></a></span>                   
+                    style="aspect-ratio: 6/4;padding: 2rem 2rem 2rem 0;object-fit: cover;">
+                <div
+                    class="home-image-actions arrows-append-mob flex-c justify-content-around justify-content-xl-start mt-4 gap-3 text-uppercase position-relative pe-5 pb-5">
+                    <span class="col-md-4">
+                        <a href="/" class="text-decoration-none small flex-c flex-wrap justify-content-xl-start"><span class="w-100 w-xl-auto text-center mt-2 mt-xl-0 order-1 order-xl-0"> Brochure Download</span>
+
+
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="23.9" class="ms-xl-2"
+                                viewBox="0 0 48 43.914">
+                                <g id="Group_921" data-name="Group 921" transform="translate(-128 -210.5)">
+                                    <path id="Path_750" data-name="Path 750"
+                                        d="M157,241h10a8,8,0,0,0,0-16,7.451,7.451,0,0,0-1.06.08,7.974,7.974,0,0,0-10.73-6.57A9.242,9.242,0,0,0,137,220.75c0,.15.02.3.02.45A10,10,0,0,0,139,241h8"
+                                        fill="none" stroke="#595959" stroke-miterlimit="10" stroke-width="2" />
+                                    <line id="Line_402" data-name="Line 402" y2="23" transform="translate(152 230)"
+                                        fill="none" stroke="#595959" stroke-miterlimit="10" stroke-width="2" />
+                                    <path id="Path_751" data-name="Path 751" d="M145,246l7,7,7-7" fill="none"
+                                        stroke="#595959" stroke-miterlimit="10" stroke-width="2" />
+                                </g>
+                            </svg>
+
+
+
+                        </a></span>
+                    <span class="col-md-4"><a href="/" class="text-decoration-none small flex-c flex-wrap justify-content-xl-start"> <span class="w-100 w-xl-auto text-center mt-2 mt-xl-0 order-1 order-xl-0">Available in Hybrid</span>
+
+
+
+                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="26" class="ms-xl-2"
+                                viewBox="0 0 46 34">
+                                <g id="Group_758" data-name="Group 758" transform="translate(-208 -136)">
+                                    <path id="Path_300" data-name="Path 300"
+                                        d="M209,154h4v5h6v7h8l3,3h13v-8h4v3h6V141h-6v4h-4v-4h-8v-4" fill="none"
+                                        stroke="#595959" stroke-miterlimit="10" stroke-width="2" />
+                                    <path id="Path_301" data-name="Path 301" d="M230,137v4h-7l-5,5h-5v5h-4" fill="none"
+                                        stroke="#595959" stroke-miterlimit="10" stroke-width="2" />
+                                    <line id="Line_141" data-name="Line 141" x2="13" transform="translate(226 137)"
+                                        fill="none" stroke="#595959" stroke-miterlimit="10" stroke-width="2" />
+                                    <line id="Line_142" data-name="Line 142" y2="22" transform="translate(209 142)"
+                                        fill="none" stroke="#595959" stroke-miterlimit="10" stroke-width="2" />
+                                    <path id="Path_302" data-name="Path 302" d="M233,152l1-7-10,10h5l-1,7,10-10Z"
+                                        fill="none" stroke="#595959" stroke-linejoin="round" stroke-miterlimit="10"
+                                        stroke-width="2" />
+                                </g>
+                            </svg>
+
+                            <!-- <i class="fs-6 text-primary fa-thin fa-camera ms-1"></i> -->
+
+
+                        </a></span>
+                    <span class="col-md-4"><a href="/" class="text-decoration-none small flex-c flex-wrap justify-content-xl-start"><span class="w-100 w-xl-auto text-center mt-2 mt-xl-0 order-1 order-xl-0">Build Your Xceed</span>
+
+
+                            <svg xmlns="http://www.w3.org/2000/svg" width="31" height="20" class="ms-xl-2"
+                                viewBox="0 0 33 20">
+                                <g id="Group_752" data-name="Group 752" transform="translate(-135.5 -50.5)">
+                                    <path id="Path_277" data-name="Path 277"
+                                        d="M162,56l-1.32-2.95A3,3,0,0,0,157.83,51H146.16a3.013,3.013,0,0,0-2.85,2.05L142,56"
+                                        fill="none" stroke="#595959" stroke-miterlimit="10" stroke-width="1" />
+                                    <path id="Path_278" data-name="Path 278" d="M147,64h10" fill="none" stroke="#595959"
+                                        stroke-miterlimit="10" stroke-width="1" />
+                                    <path id="Path_279" data-name="Path 279"
+                                        d="M167,55h-3c-.33,0-3.03,3-5,3H145c-1.97,0-4.67-3-5-3h-3a1,1,0,0,0-1,1v1.34a1,1,0,0,0,1,1l2,.66v1a3.415,3.415,0,0,0-1,2.41V64a2.97,2.97,0,0,0,1,2.22V69a1,1,0,0,0,1,1h4a1,1,0,0,0,1-1V67h14v2a1,1,0,0,0,1,1h4a1,1,0,0,0,1-1V66.22A2.986,2.986,0,0,0,166,64V62.41A3.4,3.4,0,0,0,165,60V59l2-.66a1,1,0,0,0,1-1V56A1,1,0,0,0,167,55Z"
+                                        fill="none" stroke="#595959" stroke-miterlimit="10" stroke-width="1" />
+                                    <path id="Path_280" data-name="Path 280" d="M162.59,60.38A7.32,7.32,0,0,1,158,62"
+                                        fill="none" stroke="#595959" stroke-miterlimit="10" stroke-width="1" />
+                                    <path id="Path_281" data-name="Path 281" d="M141.41,60.38A7.3,7.3,0,0,0,146,62"
+                                        fill="none" stroke="#595959" stroke-miterlimit="10" stroke-width="1" />
+                                    <line id="Line_123" data-name="Line 123" x1="4" transform="translate(141 67)"
+                                        fill="none" stroke="#595959" stroke-miterlimit="10" stroke-width="1" />
+                                    <line id="Line_124" data-name="Line 124" x1="4" transform="translate(159 67)"
+                                        fill="none" stroke="#595959" stroke-miterlimit="10" stroke-width="1" />
+                                </g>
+                            </svg>
+
+
+
+                            </i></a></span>
                 </div>
             </div>
 
